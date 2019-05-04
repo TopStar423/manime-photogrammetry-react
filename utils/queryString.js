458c1aeda3da21ee8e4b18bb16aa6d7a21b9898a
Home <home@Homes-MacBook-Pro.local>
2019-05-03 18:50:48 -0700

    save uris

--- utils/queryString.js
+++ utils/queryString.js
@@ -1,27 +1,73 @@
 import { Storage } from 'aws-amplify';
 import { setStorageBucket, setDefaultBucket } from '../components/Aws';
 
+const IMAGE_KEY_MAP = new Map();
+IMAGE_KEY_MAP.set('leftFingers', 0);
+IMAGE_KEY_MAP.set('leftThumb', 1);
+IMAGE_KEY_MAP.set('rightFingers', 2);
+IMAGE_KEY_MAP.set('rightThumb', 3);
+IMAGE_KEY_MAP.set('side', 4);
+IMAGE_KEY_MAP.set('left_fingers', 0);
+IMAGE_KEY_MAP.set('left_thumb', 1);
+IMAGE_KEY_MAP.set('right_fingers', 2);
+IMAGE_KEY_MAP.set('right_thumb', 3);
+
 const listUserFiles = async identityId => {
   const result = await Storage.list(``, { level: 'private', identityId });
   return result;
 }
 
+const getLatestKeys = userFiles => {
+  // listedKeys[0] == {key: leftFingers, lastModified: }... leftThumb, rightFingers, rightThumb, side
+  let latestKeys = [{}, {}, {}, {}, {}];
 
-const getUserFile = async s3Key => {
-  const result = await Storage.get(key, { level: 'private' }, { expires: 60 * 10 });
+  if (!Array.isArray(userFiles)) return listedKeys;
+
+  userFiles.map(item => {
+    const key = item.key;
+    if (key) {
+      // item.key is leftFingers.png -> _keyArray is [leftFingers, .png]
+      const _keyArray = key.split('.');
+      const fileName = _keyArray[0];
+
+      // If it is a valid key, check for duplicates and if newer file, replace.
+      if (IMAGE_KEY_MAP.has(fileName)) {
+        const lastModified = item.lastModified.getTime();
+        const index = IMAGE_KEY_MAP.get(fileName);
+
+        if (!latestKeys[index] || !latestKeys[index].lastModified || lastModified > latestKeys[index].lastModified) {
+          const newKey = { key, lastModified };
+          latestKeys[index] = newKey;
+        }
+      }
+    }
+  });
+  return latestKeys;
+};
+
+const getUserFile = async (s3Key, identityId) => {
+  const result = await Storage.get(s3Key, { level: 'private', identityId }, { expires: 60 * 10 });
   return result;
+}
+
+const getLatestSignedUris = async (latestKeys, identityId) => {
+  let signedUris = ['', '', '', '', ''];
+  latestKeys.map(async (item, i) => {
+    const uri = await getUserFile(item.key, identityId);
+    signedUris[i] = uri;
+  })
+  return signedUris;
 }
 
 export const getQueryString = async identityId => {
   setStorageBucket('mani-me-react-native-userfiles-1');
   let queryString = '';
   const userFiles = await listUserFiles(identityId);
-  userFiles.map(item => {
-    console.log(item.key);
-  })
-  setDefaultBucket();
-}
+  const latestKeys = getLatestKeys(userFiles);
+  const signedUris = await getLatestSignedUris(latestKeys, identityId);
 
+  console.log(signedUris);
+}
 
 
   // if (checkS3Exists($s3, $bucket, $userid, "leftFingers.jpg") == true)
